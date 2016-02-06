7a20acbe202455a10a0c5d1be22b0fd24cfca238
yura slyvka <slivka.y@gmail.com>
2016-02-06 08:56:50 +0200

    Finish layout and routes

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe StaticPagesController do
 
-  describe "GET '...'" do
+  describe "GET 'home'" do
     it "returns http success" do
-      get '...'
+      get 'home'
       response.should be_success
     end
   end
